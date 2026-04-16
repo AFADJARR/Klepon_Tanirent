75462b4cd196dc053ec613e3f89508e53f67a020
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2026-04-16 07:01:44 +0700

    public partila class form_penyewa dan fungsi form penyewa load

---
+++ Tanirent/Form Penyewa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; 
+
+namespace Tanirent
+{
+    public partial class Form_Penyewa : Form
+    {
+        Koneksi konn = new Koneksi();
+
+        public Form_Penyewa()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Penyewa_Load(object sender, EventArgs e)
+        {
+            TampilkanPenyewa();
+        }
+
+    }
+}
